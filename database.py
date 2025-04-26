a6670be94f12049fc8d0ce3a1e508307fd6294ea
Hassan Mehmood <ihassan463m@gmail.com>
2025-04-26 20:34:11 +0500

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DB_URI='postgresql://Cyclova_owner:npg_5NgAcpLuvYb0@ep-red-grass-a4jcxm9u-pooler.us-east-1.aws.neon.tech/Cyclova?sslmode=require'
+DB_URI=''
 engine = create_engine(DB_URI, echo=True)
 Base = declarative_base()
 
